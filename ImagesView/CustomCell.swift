4bb0f8cfd04d93fb195ab40a61aef1c494b30765
Drollllted <artemdrolllted@gmail.com>
2024-03-02 19:43:35 +0300

    Step 1 - Create Collection View

--- ImagesView/CustomCell.swift
+++ ImagesView/CustomCell.swift
@@ -5,4 +5,54 @@
 //  Created by Drolllted on 02.03.2024.
 //
 
-import Foundation
+import UIKit
+
+class CustomCell: UICollectionViewCell{
+    
+    static let identifire = "identifire"
+    
+    private let imagesView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "questionmark")
+        return imageView
+    }()
+    
+    private var heartButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+        button.tintColor = .red
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
+    public func configure(with image: UIImage){
+        imagesView.image = image
+        setImagesView()
+        setHeartButton()
+    }
+    
+    
+    private func setImagesView() {
+        self.addSubview(imagesView)
+        
+        NSLayoutConstraint.activate([
+            imagesView.topAnchor.constraint(equalTo: self.topAnchor),
+            imagesView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            imagesView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            imagesView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    private func setHeartButton() {
+        self.addSubview(heartButton)
+        
+        NSLayoutConstraint.activate([
+            heartButton.topAnchor.constraint(equalTo: imagesView.topAnchor, constant: 15),
+            heartButton.trailingAnchor.constraint(equalTo: imagesView.trailingAnchor, constant: -15)
+        ])
+    }
+}
